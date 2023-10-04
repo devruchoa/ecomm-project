8dbbe082a817bbb1279368bb1dde11212e206690
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2023-10-04 16:23:52 -0300

    feat: seller login form

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -11,11 +11,25 @@ import { SignUp } from '../data-types';
 export class SellerAuthComponent implements OnInit {
   constructor(private seller: SellerService, private router: Router) {}
 
+  showLogin = false;
+
   ngOnInit(): void {
     this.seller.reloadSeller();
   }
 
-  signUp(data: SignUp) {
+  signUp(data: SignUp): void {
     this.seller.userSignUp(data);
+  }
+
+  login(data: SignUp): void {
+    console.warn(data);
+  }
+
+  openLogin(): void {
+    this.showLogin = true;
+  }
+
+  openSignUp(): void {
+    this.showLogin = false;
   }
 }
